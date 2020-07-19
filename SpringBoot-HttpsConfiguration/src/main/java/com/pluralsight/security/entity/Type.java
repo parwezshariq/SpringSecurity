94aa084a977d88de8854a096882cc5a12b418cbe
parwezshariq <shariqparwez@outlook.com>
2020-07-19 11:51:14 +0530

    Https Configuration

---
+++ SpringBoot-HttpsConfiguration/src/main/java/com/pluralsight/security/entity/Type.java
@@ -0,0 +1,7 @@
+package com.shariqparwez.security.entity;
+
+public enum Type {
+
+	BUY, SELL;
+	
+}
---
+++ SpringBoot-HttpsConfiguration/src/test/java/com/pluralsight/security/TestConstants.java
@@ -0,0 +1,11 @@
+package com.shariqparwez.security;
+
+import com.shariqparwez.security.entity.CryptoCurrency;
+
+public interface TestConstants {
+
+	final CryptoCurrency bitcoin = new CryptoCurrency("BTC", "Bitcoin");
+	final CryptoCurrency litecoin = new CryptoCurrency("LTC", "Litecoin");
+
+	
+}
